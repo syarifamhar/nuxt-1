23282e45eff02c84864eb7cd1c7ddd5364fe6c88
Syarif Amhar <syarifamhar@gmail.com>
2023-05-31 18:15:10 +0700

    testing material icon

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -7,8 +7,12 @@
             <div class="p-7">
                 <h2 class="text-4xl my-7">{{ product.title }}</h2>
                 <p class="text-xl my-7">Price - ${{ product.price }}</p>
-                <h3 class="font-bold border-b-2 mb-2 pb-2">Product Description:</h3>
+                <h3 class="font-bold border-b-2 mb-2 pb-2 my-4">Product Description:</h3>
                 <p>{{ product.description }}</p>
+                <button class="btn flex my-4">
+                    <i class="material-icons mr-2">add_shopping_cart</i>
+                    <span>Add to cart</span>
+                </button>
             </div>
         </div>
     </div>
