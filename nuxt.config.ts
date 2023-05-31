9d2f82142bf32d9120e3909d4c38ddaabd40f291
Syarif Amhar <syarifamhar@gmail.com>
2023-05-31 17:57:05 +0700

    add meta and material icons config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'Learn Nuxt',
+            meta: [
+                { name: 'desription', content: 'My first step to learn of Nuxt 3' }
+            ],
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+            ]
+        }
+    }
 })
